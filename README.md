a9187ea5ae948390c201bf0239e8a71c1efd2d62
BramKn <bram@knitco.nl>
2023-04-13 17:26:02 +0200

    first

--- README.md
+++ README.md
@@ -1,47 +1,26 @@
 ![Banner image](https://user-images.githubusercontent.com/10284570/173569848-c624317f-42b1-45a6-ab09-f0ea3c247648.png)
 
-# n8n-nodes-starter
 
-This repo contains example nodes to help you get started building your own custom integrations for [n8n](n8n.io). It includes the node linter and other dependencies.
-
-To make your custom node available to the community, you must create it as an npm package, and [submit it to the npm registry](https://docs.npmjs.com/packages-and-modules/contributing-packages-to-the-registry).
-
-## Prerequisites
-
-You need the following installed on your development machine:
-
-* [git](https://git-scm.com/downloads)
-* Node.js and npm. Minimum version Node 16. You can find instructions on how to install both using nvm (Node Version Manager) for Linux, Mac, and WSL [here](https://github.com/nvm-sh/nvm). For Windows users, refer to Microsoft's guide to [Install NodeJS on Windows](https://docs.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-windows).
-* Install n8n with:
-	```
-	npm install n8n -g
-	```
-* Recommended: follow n8n's guide to [set up your development environment](https://docs.n8n.io/integrations/creating-nodes/build/node-development-environment/).
+# n8n-nodes-run-node-with-credentials-x
 
 
-## Using this starter
+## Developer
 
-These are the basic steps for working with the starter. For detailed guidance on creating and publishing nodes, refer to the [documentation](https://docs.n8n.io/integrations/creating-nodes/).
+Hi, 
 
-1. [Generate a new repository](https://github.com/n8n-io/n8n-nodes-starter/generate) from this template repository.
-2. Clone your new repo:
-    ```
-    git clone https://github.com/<your organization>/<your-repo-name>.git
-    ```
-3. Run `npm i` to install dependencies.
-4. Open the project in your editor.
-5. Browse the examples in `/nodes` and `/credentials`. Modify the examples, or replace them with your own nodes.
-6. Update the `package.json` to match your details.
-7. Run `npm run lint` to check for errors or `npm run lintfix` to automatically fix errors when possible.
-8. Test your node locally. Refer to [Run your node locally](https://docs.n8n.io/integrations/creating-nodes/test/run-node-locally/) for guidance.
-9. Replace this README with documentation for your node. Use the [README_TEMPLATE](README_TEMPLATE.md) to get started.
-10. Update the LICENSE file to use your details.
-11. [Publish](https://docs.npmjs.com/packages-and-modules/contributing-packages-to-the-registry) your package to npm.
+My name is Bram and I am the developer of this node.
+I am an independant consultant and expert partner of n8n.
+My nodes are free to use for everyone, but please consider [donating](https://donate.stripe.com/3cs5oe7xM6L77Yc5ko) when you use my nodes.
+This helps me to build and maintain nodes for everyone to use.
 
-## More information
+If you are looking for some outside help with n8n, I can of course also offer my services.
+* Node Development
+* Workflow Development
+* Mentoring
+* Support
 
-Refer to our [documentation on creating nodes](https://docs.n8n.io/integrations/creating-nodes/) for detailed information on building your own nodes.
+Please contact me @ bram@knitco.nl if you want to make use of my services.
 
-## License
+For questions or issues with nodes, please open an issue on Github.
 
-[MIT](https://github.com/n8n-io/n8n-nodes-starter/blob/master/LICENSE.md)
+
---
+++ nodes/RunNodeWithCredentialsX/RunNodeWithCredentialsX.node.ts
@@ -0,0 +1,125 @@
+import { IExecuteFunctions } from 'n8n-core';
+import {
+	IExecuteWorkflowInfo,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	IWorkflowBase,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+import subWorkflowTemplate from "./subWorkflowTemplate.json";
+
+export class RunNodeWithCredentialsX implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Run Node With Credentials X',
+		name: 'runNodeWithCredentialsX',
+		group: ['transform'],
+		version: 1,
+		icon: 'file:RunKeys.svg',
+		subtitle: '={{"CredentialId: " + $parameter["credentialsId"]}}',
+		description: 'Run any node with specified credentials',
+		defaults: {
+			name: 'Run Node WIth Credentials X',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [
+			{
+				displayName: 'Credentials ID',
+				name: 'credentialsId',
+				type: 'number',
+				default: 0,
+				required: true,
+				description: 'Id of the credentials to use',
+			},
+			{
+				displayName: 'Node To Execute',
+				name: 'nodeJson',
+				type: 'string',
+				default: '\n\n\n',
+				required: true,
+				description: 'JSON of the node to Execute',
+				typeOptions: {
+					editor: 'json',
+					rows: 20,
+				},
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		let item: INodeExecutionData;
+
+		for (let itemIndex = 0; itemIndex < 1; itemIndex++) {
+			try {
+				item = items[itemIndex];
+				const workflowInfo: IExecuteWorkflowInfo = {};
+				const nodeJson = this.getNodeParameter('nodeJson', itemIndex, '') as string;
+				const credentialsId = this.getNodeParameter('credentialsId', itemIndex, 0) as number;
+				let nodeParsed;
+				try{
+					nodeParsed = JSON.parse(nodeJson);
+					let template = JSON.parse(JSON.stringify(subWorkflowTemplate));
+
+
+					if(nodeParsed.nodes){
+						nodeParsed = nodeParsed.nodes[0];
+					}
+
+					if(nodeParsed.credentials){
+						const credentialsName = Object.keys(nodeParsed.credentials)[0];
+						nodeParsed.credentials[credentialsName].id = credentialsId.toString();
+						nodeParsed.position =[
+							1000,
+							340
+						];
+					}
+					else{
+						throw new NodeOperationError(this.getNode(), 'There are no credentials in the node that is entered.', {
+							itemIndex,
+						});
+					}
+
+					const nodeName = nodeParsed.name;
+					template.connections.Start.main[0][0]['node'] = nodeName;
+
+					template.nodes.push(nodeParsed);
+
+					workflowInfo.code = template as IWorkflowBase;
+
+					const receivedData = await this.executeWorkflow(workflowInfo, [item]);
+					return receivedData;
+
+				}
+				catch(error){
+					throw new NodeOperationError(this.getNode(), error, {
+						itemIndex,
+					});
+				}
+
+			} catch (error) {
+				// This node should never fail but we want to showcase how
+				// to handle errors.
+				if (this.continueOnFail()) {
+					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+				} else {
+					// Adding `itemIndex` allows other workflows to handle this error
+					if (error.context) {
+						// If the error thrown already contains the context property,
+						// only append the itemIndex
+						error.context.itemIndex = itemIndex;
+						throw error;
+					}
+					throw new NodeOperationError(this.getNode(), error, {
+						itemIndex,
+					});
+				}
+			}
+		}
+
+		return this.prepareOutputData(items);
+	}
+}
