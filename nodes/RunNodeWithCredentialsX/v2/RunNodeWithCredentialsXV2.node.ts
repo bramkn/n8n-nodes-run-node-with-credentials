79fd2128cf4480b06c5dcd5bd41ed323450e3df4
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-03-28 13:06:29 +0000

    Fix input and add support for multiple inputs

--- nodes/RunNodeWithCredentialsX/v2/RunNodeWithCredentialsXV2.node.ts
+++ nodes/RunNodeWithCredentialsX/v2/RunNodeWithCredentialsXV2.node.ts
@@ -1,6 +1,7 @@
 /* eslint-disable n8n-nodes-base/node-filename-against-convention */
 import { IExecuteFunctions } from 'n8n-core';
 import {
+	IDataObject,
 	IExecuteWorkflowInfo,
 	INodeExecutionData,
 	INodeType,
@@ -31,37 +32,34 @@ export class RunNodeWithCredentialsXV2 implements INodeType {
 			inputs: ['main'],
 			outputs: ['main'],
 			properties: [
-			{
-				displayName: 'Credentials ID',
+				{
+					displayName: 'Credentials ID',
 				name: 'credentialsId',
-				type: 'number',
-				default: 0,
+				type: 'string',
+				default: '',
 				required: true,
 				description: 'ID of the credentials to use',
 			},
 			{
 				displayName: 'Node To Execute',
 				name: 'nodeJson',
-				type: 'string',
-				default: '\n\n\n',
+				type: 'json',
+				default: '{\n\n}',
 				required: true,
 				description: 'JSON of the node to Execute',
-				typeOptions: {
-					editor: 'json',
-					rows: 20,
-				},
 			},
 			],
-	};
+		};
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
-
+		let returnData: INodeExecutionData[]= [];
+		for (let i = 0; i < items.length; i++) {
 			try {
 				const workflowInfo: IExecuteWorkflowInfo = {};
-				const nodeJson = this.getNodeParameter('nodeJson', 0, '') as string;
-				const credentialsId = this.getNodeParameter('credentialsId', 0, 0) as number;
+				const nodeJson = this.getNodeParameter('nodeJson', i) as string;
+				const credentialsId = this.getNodeParameter('credentialsId', i) as string;
 				let nodeParsed;
 				try{
 					nodeParsed = JSON.parse(nodeJson);
@@ -81,7 +79,7 @@ export class RunNodeWithCredentialsXV2 implements INodeType {
 						];
 					}
 					else{
-						throw new NodeOperationError(this.getNode(), 'There are no credentials in the node that is entered.');
+						throw new NodeOperationError(this.getNode(), 'There are no credentials in the node that is entered.', {itemIndex: i});
 					}
 
 					const nodeName = nodeParsed.name;
@@ -91,32 +89,37 @@ export class RunNodeWithCredentialsXV2 implements INodeType {
 
 					workflowInfo.code = template as IWorkflowBase;
 
-					const receivedData = await this.executeWorkflow(workflowInfo, items);
-					return receivedData;
+					const receivedData = await this.executeWorkflow(workflowInfo);
+
+					const executionData = this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray(receivedData[0][0].json as IDataObject[]),
+						{ itemData: { item: i } },
+					);
+
+					returnData.push(...executionData);
 
 				}
 				catch(error){
-					throw new NodeOperationError(this.getNode(), error);
+					throw new NodeOperationError(this.getNode(), error, {itemIndex: i});
 				}
 
 			} catch (error) {
 				// This node should never fail but we want to showcase how
 				// to handle errors.
 				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(0)[0].json, error, pairedItem: 0 });
+					items.push({ json: this.getInputData(0)[0].json, error, pairedItem: i });
 				} else {
 					// Adding `itemIndex` allows other workflows to handle this error
 					if (error.context) {
 						// If the error thrown already contains the context property,
 						// only append the itemIndex
-						error.context.itemIndex = 0;
+						error.context.itemIndex = i;
 						throw error;
 					}
-					throw new NodeOperationError(this.getNode(), error);
+					throw new NodeOperationError(this.getNode(), error, {itemIndex: i});
 				}
 			}
-
-
-		return this.prepareOutputData(items);
+		}
+		return [returnData];
 	}
 }
